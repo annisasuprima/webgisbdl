d28944a22839e21e8426b3fe14a965bcec478b83
annisasuprima <annisa.suprima@gmail.com>
2020-11-23 02:04:34 +0700

    upload project

---
+++ hotel_bkt/admin/inc/header.php
@@ -0,0 +1,12 @@
+
+  <header class="header black-bg" style='background-color:black;border-bottom:black;color:white; margin: 0px;'>  
+	  <div class="sidebar-toggle-box">
+		  <div class="fa fa-bars tooltips" data-placement="right" data-original-title="Toggle Navigation">
+		  </div>
+		</div>         
+    <a class="logo"><p><b>W</b>EB<b style="font-size: 17px">GIS</b>&nbspHotel (11 1811521015 Annisa Suprima)</p></a>
+      <ul class="nav pull-right top-menu">
+        <a href="act/logout.php" class="logo" style="font-size:14px"><i class="fa fa-sign-in"></i>
+        <b>Logout</b></a>
+      </ul>
+  </header>        
